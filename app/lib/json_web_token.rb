b9f61f0a8c6892ac7103caaf7e94f005658b261e
antoniolli <emailantoniolli@gmail.com>
2018-10-13 14:58:42 -0300

    correct secret_key respecting the environment

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   # secret to encode and decode token
-  HMAC_SECRET = ENV['SECRET_KEY_BASE']
+  HMAC_SECRET = Rails.env == "production" ? ENV['SECRET_KEY_BASE'] : Rails.application.credentials.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
